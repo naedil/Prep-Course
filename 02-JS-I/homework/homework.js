00df5892ad0671391f39184d09e58fa101c5adba
naedil <naedilferrer@outlook.com>
2022-11-08 18:44:11 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,10 +1,11 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "Hey"
+
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 'hey'; holis todos 
 
 // Crea una variable booleana:
 const nuevoBool = null;
